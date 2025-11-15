197cb37bfc317a60cc9c02f410aff42e21437e88
asma ezzati <asmaezzati2019@gmail.com>
2025-11-15 23:57:08 +0330

    add palette colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,18 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    colors: {
+      white: "#FFFFFF",
+      whiteSmoke: "#F5F3F4",
+      grey: "#D3D3D3",
+      silver: "#B1A7A6",
+      strawberry: "#E5383B",
+      red1: "#BA181B",
+      red2: "#BA181B",
+      darkRed: "#A4161A",
+      carbon: "#161A1D",
+      black: "#0B090A",
+    },
   },
   plugins: [],
 };
