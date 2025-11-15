61657dbeedc97bd6921c195eb67811ebbba0fc4a
asma ezzati <asmaezzati2019@gmail.com>
2025-11-15 16:31:53 +0330

    add image for background Home component

---
+++ src/components/Home.jsx
@@ -0,0 +1,10 @@
+import banner1 from "../assets/banner1.webp";
+
+const Home = () => {
+  return (
+    <>
+      <div style={{ backgroundImage: `url(${banner1})` }}></div>
+    </>
+  );
+};
+export default Home;
