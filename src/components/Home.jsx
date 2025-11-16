9423e2642e759d4f00fd826e2d13400a73d93d79
asma ezzati <asmaezzati2019@gmail.com>
2025-11-16 21:16:29 +0330

    Responsive Home Component

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,13 +1,39 @@
-import banner1 from "../assets/banner.jpeg";
+import bannerDesktop from "../assets/banner.jpeg";
+import bannerMobile from "../assets/banner2.webp";
 import Navbar from "./Navbar";
 
 const Home = () => {
   return (
     <>
-      <div
-        className="h-screen  bg-cover bg-center bg-no-repeat bg-fixed opacity-90 relative"
-        style={{ backgroundImage: `url(${banner1})` }}
-      >
+      <div className="h-screen bg-cover bg-center bg-no-repeat bg-fixed opacity-90 relative">
+        {/* desktop */}
+        <div className="hidden lg:block absolute inset-0">
+          <img
+            src={bannerDesktop}
+            alt="Background Desktop"
+            className="w-full h-full object-cover"
+          />
+        </div>
+
+        {/* mobile */}
+        <div className="md:hidden absolute inset-0 z-0">
+          <img
+            src={bannerMobile}
+            alt="Background Mobile"
+            className="w-full h-full object-cover"
+          />
+        </div>
+
+        {/* tablet */}
+        <div className="hidden md:block lg:hidden absolute inset-0 z-0">
+          <img
+            src={bannerMobile} /* قبلاً دوباره bannerMobile گذاشته بودی */
+            alt="Background Tablet"
+            className="w-full h-full object-cover"
+          />
+        </div>
+
+        {/* Navbar را بالاتر از تصاویر قرار می‌دهیم */}
         <Navbar />
       </div>
     </>
