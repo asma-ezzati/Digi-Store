77299a78b06039d084c060b191148ea88bb19fc0
asma ezzati <asmaezzati2019@gmail.com>
2025-11-16 14:27:51 +0330

    add new image for background Home component

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,9 +1,15 @@
-import banner1 from "../assets/banner1.webp";
+import banner1 from "../assets/banner.jpeg";
 
 const Home = () => {
   return (
     <>
-      <div style={{ backgroundImage: `url(${banner1})` }}></div>
+      <div
+        className="h-screen  bg-cover bg-center bg-no-repeat bg-fixed opacity-90 "
+        style={{ backgroundImage: `url(${banner1})` }}
+      >
+        {/* <img className="col-span-2" src={banner1}></img> */}
+        <div></div>
+      </div>
     </>
   );
 };
