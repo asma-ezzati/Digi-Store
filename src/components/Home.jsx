1130239cff02fec78d04b1a3c55c0ad3b2e49d23
asma ezzati <asmaezzati2019@gmail.com>
2025-11-16 15:13:45 +0330

    style Navbar

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -4,16 +4,26 @@ const Home = () => {
   return (
     <>
       <div
-        className="h-screen  bg-cover bg-center bg-no-repeat bg-fixed opacity-90 "
+        className="h-screen  bg-cover bg-center bg-no-repeat bg-fixed opacity-90 relative"
         style={{ backgroundImage: `url(${banner1})` }}
       >
         {/* <img className="col-span-2" src={banner1}></img> */}
-        <nav className="flex p-2 mx-auto ">
-          <a className="mx-2">خانه</a>
-          <a className="mx-2">درباره ما</a>
-          <a className="mx-2">فروشگاه</a>
-          <a className="mx-2">تماس با ما</a>
-          <a className="mx-2">ثبت نام</a>
+        <nav className=" flex py-4 mx-5 rounded-full  bg-[rgba(255,255,255,0.08)] bg-blur-[10px] absolute top-5 ">
+          <a className=" text-center  mx-5 font-Vazir text-lg font-bold text-carbon hover:text-red1 ">
+            خانه
+          </a>
+          <a className=" text-center  mx-5 font-Vazir text-lg font-bold text-carbon hover:text-red1 ">
+            درباره ما
+          </a>
+          <a className=" text-center  mx-5 font-Vazir text-lg font-bold text-carbon hover:text-red1 ">
+            فروشگاه
+          </a>
+          <a className=" text-center  mx-5 font-Vazir text-lg font-bold text-carbon  hover:text-red1">
+            تماس با ما
+          </a>
+          <a className=" text-center  mx-5 font-Vazir text-lg font-bold text-carbon  hover:text-red1">
+            ثبت نام
+          </a>
         </nav>
       </div>
     </>
