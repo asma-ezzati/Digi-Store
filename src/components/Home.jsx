86763c7d3fa4dd61253fa9afaa09ad796efd12d1
asma ezzati <asmaezzati2019@gmail.com>
2025-11-16 18:52:25 +0330

    Separate Navbar logic into its own component

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,4 +1,5 @@
 import banner1 from "../assets/banner.jpeg";
+import Navbar from "./Navbar";
 
 const Home = () => {
   return (
@@ -7,24 +8,7 @@ const Home = () => {
         className="h-screen  bg-cover bg-center bg-no-repeat bg-fixed opacity-90 relative"
         style={{ backgroundImage: `url(${banner1})` }}
       >
-        {/* <img className="col-span-2" src={banner1}></img> */}
-        <nav className=" flex py-4 mx-5 rounded-full  bg-[rgba(255,255,255,0.08)] bg-blur-[10px] absolute top-5 ">
-          <a className=" text-center  mx-5 font-Vazir text-lg font-bold text-carbon hover:text-red1 ">
-            خانه
-          </a>
-          <a className=" text-center  mx-5 font-Vazir text-lg font-bold text-carbon hover:text-red1 ">
-            درباره ما
-          </a>
-          <a className=" text-center  mx-5 font-Vazir text-lg font-bold text-carbon hover:text-red1 ">
-            فروشگاه
-          </a>
-          <a className=" text-center  mx-5 font-Vazir text-lg font-bold text-carbon  hover:text-red1">
-            تماس با ما
-          </a>
-          <a className=" text-center  mx-5 font-Vazir text-lg font-bold text-carbon  hover:text-red1">
-            ثبت نام
-          </a>
-        </nav>
+        <Navbar />
       </div>
     </>
   );
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,2 +1,24 @@
-const Navbar = () => {};
+const Navbar = () => {
+  return (
+    <>
+      <nav className=" flex py-4 mx-5 rounded-full  bg-[rgba(255,255,255,0.08)] bg-blur-[10px] absolute top-5 ">
+        <a className=" text-center  mx-5 font-Vazir text-lg font-bold text-carbon hover:text-red1 ">
+          خانه
+        </a>
+        <a className=" text-center  mx-5 font-Vazir text-lg font-bold text-carbon hover:text-red1 ">
+          درباره ما
+        </a>
+        <a className=" text-center  mx-5 font-Vazir text-lg font-bold text-carbon hover:text-red1 ">
+          فروشگاه
+        </a>
+        <a className=" text-center  mx-5 font-Vazir text-lg font-bold text-carbon  hover:text-red1">
+          تماس با ما
+        </a>
+        <a className=" text-center  mx-5 font-Vazir text-lg font-bold text-carbon  hover:text-red1">
+          ثبت نام
+        </a>
+      </nav>
+    </>
+  );
+};
 export default Navbar;
