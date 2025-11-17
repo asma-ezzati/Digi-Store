387c0bcaedbb258cf4883b43ea914bedf98c4005
asma ezzati <asmaezzati2019@gmail.com>
2025-11-17 19:35:05 +0330

    style store component

--- src/components/store.jsx
+++ src/components/store.jsx
@@ -1,7 +1,55 @@
+import Laptop from "../assets/products/Laptop.webp";
+import Mouse from "../assets/products/Mouse.webp";
+import Keyboard from "../assets/products/Keyboard.webp";
+import Headphone from "../assets/products/Headphone.webp";
+
 const Store = () => {
   return (
     <>
-      <div className="pt-24 pb-16"></div>
+      <div className="pt-24 pb-16 bg-whiteSmoke">
+        <div className="grid lg:grid-cols-4  w-[95%] mx-auto ">
+          <div className=" bg-white m-3 rounded-xl shadow-xl ">
+            <img
+              src={Laptop}
+              alt="Laptop"
+              className="p-2 w-[70%] mx-auto"
+            ></img>
+            <h1 className="font-Vazir text-xl text-center ">لپ تاپ</h1>
+            <button className=" p-2 m-2 mt-4 rounded-xl w-[94%] font-Vazir bg-strawberry text-white  hover:bg-darkRed ">
+              همه ی محصولات
+            </button>
+          </div>
+          <div className=" bg-white m-3 rounded-xl shadow-xl  ">
+            <img src={Mouse} alt="Mouse" className="p-2 w-[70%] mx-auto"></img>
+            <h1 className="font-Vazir text-xl text-center ">موس</h1>
+            <button className=" p-2 m-2 mt-4 rounded-xl w-[94%] font-Vazir bg-strawberry text-white  hover:bg-darkRed  ">
+              همه ی محصولات
+            </button>
+          </div>
+          <div className=" bg-white m-3 rounded-xl shadow-xl">
+            <img
+              src={Keyboard}
+              alt="Keyboard"
+              className="p-2 w-[70%] mx-auto"
+            ></img>
+            <h1 className="font-Vazir text-xl text-center "> کیبورد</h1>
+            <button className=" p-2 m-2 mt-4 rounded-xl w-[94%] font-Vazir bg-strawberry text-white  hover:bg-darkRed ">
+              همه ی محصولات
+            </button>
+          </div>
+          <div className=" bg-white m-3 rounded-xl shadow-xl">
+            <img
+              src={Headphone}
+              alt="Headphone"
+              className="p-2 w-[70%] mx-auto"
+            ></img>
+            <h1 className="font-Vazir text-xl text-center ">هدفون </h1>
+            <button className=" p-2 m-2 mt-4 rounded-xl w-[94%] font-Vazir bg-strawberry text-white  hover:bg-darkRed ">
+              همه ی محصولات
+            </button>
+          </div>
+        </div>
+      </div>
     </>
   );
 };
