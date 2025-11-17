2b32775ef403dfaaada12b9c69109245b5d3d27a
asma ezzati <asmaezzati2019@gmail.com>
2025-11-17 17:35:40 +0330

    import Store component in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import Aboutus from "./components/Aboutus";
 import Home from "./components/Home";
+import Store from "./components/store";
 
 const App = () => {
   return (
     <>
       <Home />
       <Aboutus />
+      <Store />
     </>
   );
 };
